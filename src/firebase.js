5db24d43c09e60f4e1a068ce7c8c0ad9a51428fe
Siddhesh Palimkar <sidspalimkar07@gmail.com>
2021-06-06 11:18:57 +0530

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBtxhFo6U158Mr5SLHSILIZaLQiv4LTtE0",
-  authDomain: "discord-clone-a8778.firebaseapp.com",
-  projectId: "discord-clone-a8778",
-  storageBucket: "discord-clone-a8778.appspot.com",
-  messagingSenderId: "43399741815",
-  appId: "1:43399741815:web:f15c4ed4d37ecf57aaa172",
-};
+
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
